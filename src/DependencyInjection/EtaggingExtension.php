ba14cf8509b28f7561e7d78bdc91515af35d2f0a
Arashdeep Singh <arash@delhiit.com>
2020-05-12 15:05:09 +0530

    extension fix 3

--- src/DependencyInjection/EtaggingExtension.php
+++ src/DependencyInjection/EtaggingExtension.php
@@ -11,7 +11,7 @@ namespace Adsazad\EtaggingBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
 
 /**
  * Description of EtaggingExtension
