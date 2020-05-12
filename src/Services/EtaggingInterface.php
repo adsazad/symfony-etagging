bc79f493cde33d94383d42934551a72843a57ff4
Arashdeep Singh <arash@delhiit.com>
2020-05-12 15:54:24 +0530

    namespac

--- src/Services/EtaggingInterface.php
+++ src/Services/EtaggingInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace adsazad\SymfonyEtaggingBundle;
+namespace adsazad\SymfonyEtaggingBundle\Services;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
